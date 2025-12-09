08447d6c630cf802cd4c42f2705b9b88477322d5
Hugo-CHARLES <hugo.charles@etu.univ-smb.fr>
2025-12-09 22:07:48 +0100

    Création de RessourceManager

---
+++ Radfall/RessourceManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Radfall
+{
+    internal class RessourceManager
+    {
+
+        private Dictionary<string, BitmapImage> images = new Dictionary<string, BitmapImage>();
+
+        public void LoadImage(string imgName, string filename)
+        {
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(filename);
+
+            // Charger l'image en mémoire
+            // Garde l'image en mémoire pour pouvoir y accéder rapidement
+            image.CacheOption = BitmapCacheOption.OnLoad;
+
+            images.Add(imgName, image);
+
+            image.EndInit();
+        }
+
+        public BitmapImage GetImage(string imgName)
+        {
+            return images[imgName];
+        }
+
+    }
+}
