476485edcdf4f0581aad48515423e632429ebd16
Hugo-CHARLES <hugo.charles@etu.univ-smb.fr>
2025-12-12 13:48:58 +0100

    Nouvelle architecture projet

---
+++ Radfall/game/Poison.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace Radfall.game
+{
+    internal class Poison : Drawable
+    {
+
+        public double damage { get; set; }
+
+        public double accoutumance { get; set; }
+
+        public Poison (double x, double y, Image img) : base (x, y, img)
+        {
+                
+        }
+
+
+
+    }
+}
