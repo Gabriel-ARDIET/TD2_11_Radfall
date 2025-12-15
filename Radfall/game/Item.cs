bdbe430c6126e6567cfd315d01d4678fd9be6aa8
Hugo-CHARLES <hugo.charles@etu.univ-smb.fr>
2025-12-15 09:14:45 +0100

    Creation class Item, permet d'avoir des entity récupérable

---
+++ Radfall/game/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Radfall.game
+{
+    internal class Item : Entity
+    {
+        public Item(double x, double y, Image img): base(x, y, img)
+        {
+            this.x = x;
+            this.y = y;
+            this.img = img;
+        }
+
+        public virtual void Grab(Player player) { }
+    }
+}
