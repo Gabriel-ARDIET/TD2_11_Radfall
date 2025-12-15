97fae52b54dabc1c59a1e75bc6abcd3284d3f616
Hugo-CHARLES <hugo.charles@etu.univ-smb.fr>
2025-12-15 11:34:40 +0100

    Ajout de RespawnTime dans Item

--- Radfall/game/Item.cs
+++ Radfall/game/Item.cs
@@ -9,11 +9,14 @@ namespace Radfall.game
 {
     internal class Item : Entity
     {
+        public uint RespawnTime = 60;
+
         public Item(double x, double y, Image img, EntityManager entityManager): base(x, y, img, entityManager)
         {
             this.x = x;
             this.y = y;
             this.img = img;
+            IsVisible = true;
         }
 
         public virtual void Grab(Player player) { }
