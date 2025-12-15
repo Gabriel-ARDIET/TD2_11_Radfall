23ce142240ff20af3b1dd7c4a8e8566048038a29
Gabriel-ARDIET <gabriel.ardiet@etu.univ-smb.fr>
2025-12-15 09:19:16 +0100

    Ajout méthodes dans la class Poison

--- Radfall/game/Poison.cs
+++ Radfall/game/Poison.cs
@@ -8,19 +8,42 @@ using System.Xml.Linq;
 
 namespace Radfall.game
 {
-    internal class Poison : Drawable
+    internal class Poison : Entity
     {
+        private const double POISON_INTERVAL = 1;
+        public int Amount { get; set; }
+        double timer = 0;
 
-        public double damage { get; set; }
-
-        public double accoutumance { get; set; }
-
-        public Poison (double x, double y, Image img) : base (x, y, img)
+        public Poison(double x, double y, Image img, EntityManager entityManager, int amount) : base (x, y, img, entityManager)
         {
-                
+            this.x = x;
+            this.y = y;
+            this.Amount = amount;
         }
 
+        public override void Update(double dTime)
+        {
+            timer += dTime;
+            if (timer >= POISON_INTERVAL)
+            {
+                timer = 0;
+                CheckEntities();
+            }
+        }
 
-
+        internal void CheckEntities()
+        {
+            foreach (var entity in entityManager.Entities)
+            {
+                if (entity is Player)
+                {
+                    if (Hitbox.IntersectsWith(entity.Hitbox))
+                    {
+                        Player player = entity as Player;
+                        player.TakePoison(Amount);
+                    }
+                }
+            }
+        }
     }
 }
